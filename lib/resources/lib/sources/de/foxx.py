1bffed77b3b522106e89fbf31db168b172976fd2
Lastship <lastship@users.noreply.github.com>
2018-03-06 19:10:25 +0100

    downgrade

--- lib/resources/lib/sources/de/foxx.py
+++ lib/resources/lib/sources/de/foxx.py
@@ -18,14 +18,21 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import base64
+import json
 import re
 import urllib
 import urlparse
+import requests
+
 
 from resources.lib.modules import anilist
+from resources.lib.modules import cache
 from resources.lib.modules import cleantitle
 from resources.lib.modules import client
+from resources.lib.modules import directstream
 from resources.lib.modules import dom_parser
+from resources.lib.modules import jsunpack
 from resources.lib.modules import source_utils
 from resources.lib.modules import tvmaze
 
@@ -35,8 +42,9 @@ class source:
         self.language = ['de']
         self.domains = ['foxx.to']
         self.base_link = 'http://foxx.to'
-        self.search_link = '/?s=%s'
-
+        self.search_link = '/wp-json/dooplay/search/?keyword=%s&nonce=%s'
+        
+        
     def movie(self, imdb, title, localtitle, aliases, year):
         try:
             url = self.__search([localtitle] + source_utils.aliases_to_array(aliases), year)
@@ -82,43 +90,48 @@ class source:
 
             url = urlparse.urljoin(self.base_link, url)            
             r = client.request(url, output='extended')
-
+            regex= ur'iframe src="(.+?)"+?'
+            url_iframe=re.findall(regex,r[0])
+            r = client.request(url_iframe[0], output='extended')
             headers = r[3]
             headers.update({'Cookie': r[2].get('Set-Cookie'), 'Referer': self.base_link})
-            r = r[0]
-
-            link = re.findall('iframe src="(.*?)"', r)[0]
-            r = client.request(link, headers=headers, referer=url)
-            links = re.findall('file":"(.*?)","label":"(.*?)"', r)
-
-            try:
-                for url, quality in links:
-                    sources.append(
-                        {'source': 'CDN', 'quality': quality, 'language': 'de', 'url': url, 'direct': True,
-                         'debridonly': False})
-            except:
-                pass
-
+            r = r[0]     
+            links=re.findall('''(?:link|file)["']?\s*:\s*["'](.+?)["']''', r) 
+            url2redirect=links[3]            
+            final_url_redirected = requests.get(url2redirect,headers=headers, allow_redirects=False)
+            url = final_url_redirected.headers['Location']
+            # quick&dirty fix Nov2017 - sources.py had to be changed to make this compatible    
+            sources.append({'source': 'CDN', 'quality': '1080p', 'language': 'de', 'url':url+'|%s' % urllib.urlencode(headers) , 'direct': True, 'debridonly': False})
+            
             return sources
         except:
             return sources
 
     def resolve(self, url):
-        url = client.request(url, redirect=False)
-        return re.findall('http.*', url)[0]
+        return url
 
     def __search(self, titles, year):
         try:
-            query = self.search_link % (urllib.quote_plus(cleantitle.query(titles[0])))
+            n = cache.get(self.__get_nonce, 24)
+
+            query = self.search_link % (urllib.quote_plus(cleantitle.query(titles[0])), n)
             query = urlparse.urljoin(self.base_link, query)
 
+            t = [cleantitle.get(i) for i in set(titles) if i]
+            y = ['%s' % str(year), '%s' % str(int(year) + 1), '%s' % str(int(year) - 1), '0']
+
             r = client.request(query)
-            r = re.findall('href="(.*-Stream)', r)
+            r = json.loads(r)
+            r = [(r[i].get('url'), r[i].get('title'), r[i].get('extra').get('date')) for i in r]
+            r = sorted(r, key=lambda i: int(i[2]), reverse=True)  # with year > no year
+            r = [i[0] for i in r if cleantitle.get(i[1]) in t and i[2] in y][0]
 
-            r = [(i, re.findall('\d{4}', i, )[0]) for i in r]
-            r = sorted(r, key=lambda i: int(i[1]), reverse=True)  # with year > no year
-            r = [x[0] for x in r if int(x[1]) == int(year)]
-
-            return source_utils.strip_domain(r[0])
+            return source_utils.strip_domain(r)
         except:
             return
+
+    def __get_nonce(self):
+        n = client.request(self.base_link)
+        try: n = re.findall('nonce"?\s*:\s*"?([0-9a-zA-Z]+)', n)[0]
+        except: n = '5d12d0fa54'
+        return n
