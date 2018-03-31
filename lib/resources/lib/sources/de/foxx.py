24d28f21f2c32b35717e514afb3bcc01a5e3a515
Lastship <lastship@users.noreply.github.com>
2018-03-31 12:48:56 +0200

    Update foxx.py

--- lib/resources/lib/sources/de/foxx.py
+++ lib/resources/lib/sources/de/foxx.py
@@ -23,12 +23,12 @@ import urllib
 import urlparse
 
 from resources.lib.modules import anilist
-from resources.lib.modules import cleantitle
 from resources.lib.modules import client
 from resources.lib.modules import dom_parser
 from resources.lib.modules import source_utils
 from resources.lib.modules import tvmaze
 from resources.lib.modules import source_faultlog
+from resources.lib.modules import cfscrape
 
 class source:
     def __init__(self):
@@ -37,6 +37,7 @@ class source:
         self.domains = ['foxx.to']
         self.base_link = 'http://foxx.to'
         self.search_link = '/?s=%s'
+        self.scraper = cfscrape.create_scraper()
 
     def movie(self, imdb, title, localtitle, aliases, year):
         try:
@@ -46,34 +47,34 @@ class source:
             
             return url
         except:
-            return
+            return ""
 
     def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):
         try:
-            url = self.__search([localtvshowtitle] + source_utils.aliases_to_array(aliases), year)
-            if not url and tvshowtitle != localtvshowtitle: url = self.__search([tvshowtitle] + source_utils.aliases_to_array(aliases), year)
-            if not url and source_utils.is_anime('show', 'tvdb', tvdb): url = self.__search([tvmaze.tvMaze().showLookup('thetvdb', tvdb).get('name')] + source_utils.aliases_to_array(aliases), year)
-            return url
+
+            linkAndTitle = self.__search([tvshowtitle, localtvshowtitle] + source_utils.aliases_to_array(aliases), year)
+            aliases = source_utils.aliases_to_array(aliases)
+
+            if not tvshowtitle and source_utils.is_anime('show', 'tvdb', tvdb): linkAndTitle = self.__search([tvmaze.tvMaze().showLookup('thetvdb', tvdb).get('name')] + source_utils.aliases_to_array(aliases), year)
+
+            return linkAndTitle
         except:
-            return
+            return ""
 
     def episode(self, url, imdb, tvdb, title, premiered, season, episode):
         try:
             if not url:
                 return
-
-            url = urlparse.urljoin(self.base_link, url)
-            url = client.request(url, output='geturl')
+            r = self.scraper.get(url).content
 
             if season == 1 and episode == 1:
                 season = episode = ''
 
-            r = client.request(url)
             r = dom_parser.parse_dom(r, 'ul', attrs={'class': 'episodios'})
             r = dom_parser.parse_dom(r, 'a', attrs={'href': re.compile('[^\'"]*%s' % ('-%sx%s' % (season, episode)))})[0].attrs['href']
             return source_utils.strip_domain(r)
         except:
-            return
+            return ""
 
     def sources(self, url, hostDict, hostprDict):
         sources = []
@@ -81,29 +82,41 @@ class source:
             if not url:
                 return sources
 
-            url = urlparse.urljoin(self.base_link, url)            
-            r = client.request(url, output='extended')
+            url = urlparse.urljoin(self.base_link, url)
+            r = self.scraper.get(url).content
 
-            headers = r[3]
-            headers.update({'Cookie': r[2].get('Set-Cookie'), 'Referer': self.base_link})
-            r = r[0]
+            link = dom_parser.parse_dom(r, 'iframe', attrs={'width': '100%'})[0].attrs['src']
+            r = self.scraper.get(link).content
+            phrase = re.findall('jbdaskgs = \'(.*)?\'', r)[0]
+            links = self._decodePhrase(phrase)
 
-            link = re.findall('iframe src="(.*?)"', r)[0]
-            r = client.request(link, headers=headers, referer=url)
-            links = re.findall('file":"(.*?)","label":"(.*?)"', r)
-
-            try:
-                for url, quality in links:
-                    sources.append(
-                        {'source': 'CDN', 'quality': quality, 'language': 'de', 'url': url, 'direct': True,
-                         'debridonly': False})
-            except:
-                pass
+            [sources.append({'source': 'CDN', 'quality': i['label'], 'language': 'de', 'url': i['file'], 'direct': True,
+                         'debridonly': False}) for i in links]
 
             return sources
         except:
             source_faultlog.logFault(__name__,source_faultlog.tagScrape)
             return sources
+
+    def _decodePhrase(self, phrase):
+        import json
+        Array = list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/')
+        b = 0
+        l = 0
+        r = ''
+        for count, i in enumerate(phrase):
+            c = Array.index(i)
+            b = (b << 6) + c
+            l += 6
+
+            while l >= 8:
+                l -= 8
+
+                a = (b >> l if b >= 0 else (b + 0x100000000) >> l) & 0xff
+                if count < len(phrase):
+                    r = r + unichr(a)
+
+        return json.loads(r)
 
     def resolve(self, url):
         url = client.request(url, redirect=False)
@@ -114,19 +127,18 @@ class source:
             query = self.search_link % (urllib.quote_plus(titles[0]))
             query = urlparse.urljoin(self.base_link, query)
 
-            r = client.request(query)
+            r = self.scraper.get(query).content
             dom_parsed = dom_parser.parse_dom(r, 'div', attrs={'class': 'details'})
+            links = [(dom_parser.parse_dom(i, 'a')[0], dom_parser.parse_dom(i, 'span', attrs={'class' : 'year'})[0].content) for i in dom_parsed]
 
-            r = [(re.findall('href="(.*-Stream)', i.content)[0], re.findall('year">(\d*)', i.content, )[0]) for i in dom_parsed if len(re.findall('de\.png', i.content)) > 0]
+            r = sorted(links, key=lambda i: int(i[1]), reverse=True)  # with year > no year
+            r = [x[0].attrs['href'] for x in r if int(x[1]) == int(year)]
 
-            r = sorted(r, key=lambda i: int(i[1]), reverse=True)  # with year > no year
-            r = [x[0] for x in r if int(x[1]) == int(year)]
-
-            if len(r) > 0 :
-                return source_utils.strip_domain(r[0])
+            if len(r) > 0:
+                return r[0]
 
             return ""
 
         except:
             source_faultlog.logFault(__name__, source_faultlog.tagSearch)
-            return
+            return ""
